964291bf5ce169d93185956541c49a71ae3b9e3e
mayalmog <mayalmog@gmail.com>
2022-08-29 20:49:03 +0300

    feature: login-page basic setup

--- src/views/login-page.jsx
+++ src/views/login-page.jsx
@@ -1,3 +1,57 @@
+import { useState } from "react";
 export const LoginPage = () => {
-  return <h3>Login Page</h3>;
+  const [user, setUser] = useState(null);
+  const [admin, setAdmin] = useState({ email: "", password: "" });
+
+  const handleChange = ({ target }) => {
+    const { value } = target;
+    setUser(value);
+  };
+
+  const handleChangeAdmin = ({ target }) => {
+    const { value } = target;
+    if (target.name === "adminEmail") {
+      setAdmin({ ...admin, email: value });
+    }
+    if (target.name === "adminPassword") {
+      setAdmin({ ...admin, password: value });
+    }
+  };
+
+  return (
+    <section className="login-page flex column justify-center align-center">
+      <p>curren user:</p>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <p>curren admin:</p>
+      <pre>{JSON.stringify(admin, null, 2)}</pre>
+      <h1>Login to your account</h1>
+      <form className="flex column justify-center align-center">
+        <label htmlFor="users">Choose you name:</label>
+        <select name="users" id="users" onChange={handleChange}>
+          <option value="default">Choose user:</option>
+          <option value="May Almog">May Almog</option>
+          <option value="Ziv Bryk">Ziv Bryk</option>
+        </select>
+        <button>Login</button>
+      </form>
+
+      <hr />
+      <form className="flex column justify-center align-center">
+        <h5>Or login as Admin:</h5>
+        <input
+          type="email"
+          name="adminEmail"
+          placeholder="Email"
+          onChange={handleChangeAdmin}
+        />
+        <input
+          type="password"
+          name="adminPassword"
+          placeholder="Password"
+          onChange={handleChangeAdmin}
+        />
+        <button>Login as Admin</button>
+      </form>
+    </section>
+  );
 };
